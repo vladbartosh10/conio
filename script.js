fd0035c5c3a417d621908e728961c3943d48cfb8
Vladislav <vladbartosh20@gmail.com>
2023-03-14 16:11:53 +0300

    First project

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privet: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из последних просмотреных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
